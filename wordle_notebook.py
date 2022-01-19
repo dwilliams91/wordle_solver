9c01aea9c827b0d1ff39968d79da36cc0f3df920
David Williams <davidwilliams@ip-192-168-86-152.ec2.internal>
2022-01-19 07:08:44 -0600

    remove algorithm works but is messy

--- wordle_notebook.py
+++ wordle_notebook.py
@@ -47,64 +47,88 @@ def solve_worldle(sample_list):
     best_third=find_top_5(letter_dictionary_third)
     best_fourth=find_top_5(letter_dictionary_fourth)
     best_fifth=find_top_5(letter_dictionary_fifth)
-    return(best_first, best_second, best_third, best_fourth, best_fifth)
+    return [best_first, best_second, best_third, best_fourth, best_fifth]
 
 
 # %%
-def refine_list(correct_position_letters, incorrect_position_letters, sample_list):
-    remove_list=[]
-    remove_list_2=[]
-    correct_key_list_position=correct_position_letters.keys()
-    incorrect_key_list_position=incorrect_position_letters.keys()
-    incorrect_values_list=incorrect_position_letters.values()
+def refine_list(green_letters, yellow_letters, black_letters, sample_list):
+    green_remove_list=[]
+    yellow_remove_list=[]
+    black_remove_list=[]
+
+    correct_key_list_position=list(green_letters.keys())
+    incorrect_key_list_position=list(yellow_letters.keys())
+    incorrect_values_list=yellow_letters.values()
+
+    black_letters_list=black_letters.values()
+
+
     modified_sample_list=sample_list
 
-    if len(correct_position_letters) !=0:
+    if len(green_letters) !=0:
         for x in range(0,len(sample_list)):
             word=sample_list[x]
             need_to_be_removed=False
             # if the right letter is in the right spot
             for position in correct_key_list_position:
-                if word[position] != correct_position_letters[position]:
+                if word[position] != green_letters[position]:
                     need_to_be_removed=True
                     break
             # if the right letter is in the wrong spot, remove words where the letter is in that spot
             for position in incorrect_key_list_position:
-                if word[position]==incorrect_key_list_position[position]:
+                if word[position]==yellow_letters[position]:
                     need_to_be_removed=True
                     break
             if need_to_be_removed:
-                remove_list.append(word)
+                green_remove_list.append(word)
 
-        for word in remove_list:
+        for word in green_remove_list:
             modified_sample_list.remove(word)
 
-    # taking out words that don't contain the letters
+    # taking out words that don't contain the letters 
     for word in sample_list:
         for letter in incorrect_values_list:
             if letter not in word:
-                remove_list_2.append(word)
+                yellow_remove_list.append(word)
     
-    for word in remove_list_2:
+    for word in yellow_remove_list:
         if word in modified_sample_list:
             modified_sample_list.remove(word)
+
+    #take out words that dont have black letters
+    for x in range(0,len(sample_list)):
+        word=sample_list[x]
+        for letter in word:
+            if letter in black_letters_list:
+                black_remove_list.append(word)
+    
+    for word in black_remove_list:
+        if word in modified_sample_list:
+            modified_sample_list.remove(word)
+
+
     
     return modified_sample_list
 
 correct_position_letters_dict={
-     
+     4:"s"
 }
 incorrect_position_letters={
-    2:"t", 
-    4:"h"}
+    3:"e"
+}
+incorrect_letters={
+    0:"c",
+    1:"o",
+    2:"r"
 
+}
 
-refined_list=refine_list(correct_position_letters_dict, incorrect_position_letters, sample_list)
+refined_list=refine_list(correct_position_letters_dict, incorrect_position_letters, incorrect_letters, sample_list)
 
 
 # %%
 # best_first, best_second, best_third, best_fourth, best_fifth=solve_worldle(refined_list)
-best_letters_to_guess=solve_worldle(sample_list)
+best_letters_to_guess=solve_worldle(refined_list)
 best_first, best_second, best_third, best_fourth, best_fifth=best_letters_to_guess
 print("best first letter", best_first)
 print("best second letter", best_second)
@@ -114,16 +138,40 @@ print("best fifth letter",best_fifth)
 # %%
 
 def find_best_word(best_letters_to_guess, sample_list):
-    best_letter_for_each_position=[]
-    for x in range(0, len(best_letters_to_guess)):
-        best_letter_for_each_position.append(best_letters_to_guess[x][0])
-    def find_max(list_of_touples):
-        occurance_of_letters=[]
-        for item in list_of_touples:
-            occurance_of_letters.append(item[0])
-        occurance_of_letters.sort(reverse=True)
-        return occurance_of_letters
-# option 1- pick best letter, then 
+    print(best_letters_to_guess)    
+    
+    # what the data looks like
+    # [
+    # [('c', 410), ('b', 383), ('a', 243), ('s', 193), ('f', 184)],
+    #  [('o', 385), ('a', 368), ('r', 274), ('e', 272), ('i', 261)], 
+    # [('a', 298), ('i', 257), ('o', 235), ('r', 205), ('e', 191)], 
+    # [('e', 509), ('t', 178), ('n', 172), ('l', 171), ('a', 140)], 
+    # [('s', 728), ('e', 327), ('y', 220), ('t', 197), ('r', 164)]
+    # ]
+
+    # option 1- refine the list down to one word 
+    # 1) find the most important letter to guess and its position: s as the 5th letter with 728
+    # 2) go through sample list and eliminate all words that don't have the letter in that spot
+
+    # 3) find the next most important: e as the 4th letter with 509. 
+    # 4) go through and make sure that there are words left with that letter in that spot
+    # 5) if there are no words of that letter in that spot left, find the next best letter: o as 385
+    # 6) remove sample list and elimate all words that don't have the letter in that spot
+    # 7) repeat steps 3-6 until you have filled in all 5 letters, thus finding the most likely best word to guess
+    # note: it won't always be the first letter. The first guess would be S in 5, E in 4, C in 1, O in 2, but R in 3 to make the word CORES
+
+    # 
+
+    # option 2- make the best score
+    # 1)
+    
+
+    # option 3- try to get the most correct letters in the incorrect spots
+
+
+
 
 find_best_word(best_letters_to_guess, sample_list)
+
+
 # %%
